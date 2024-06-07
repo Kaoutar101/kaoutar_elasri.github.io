3e71a5330c90e9c5a0dac675f824f14d158a3e21
Kaoutar101 <140347595+Kaoutar101@users.noreply.github.com>
2024-06-07 12:19:53 +0100

    Update Hotel.js

--- hotel_agadir/Hotel.js
+++ hotel_agadir/Hotel.js
@@ -2,19 +2,7 @@ const Account = require('./Account');
 const Client = require('./Client');
 const Manager = require('./Manager');
 const Room = require('./Room');
-
-// Function to save user information to local storage
-function saveUserInfo(email, password) {
-    localStorage.setItem('email', email);
-    localStorage.setItem('password', password);
-}
-
-// Function to retrieve user information from local storage
-function getUserInfo() {
-    const email = localStorage.getItem('email');
-    const password = localStorage.getItem('password');
-    return { email, password };
-}
+const prompt=require('prompt-sync')({sigint: true})
 
 let email = null;
 let password = null;
@@ -25,7 +13,33 @@ let answer = prompt("type your choice\n");
 const account = new Account(email, password);
 
 if (answer == 1) {
-    // Manager login logic
+    let accnt=false;
+    let i=0;
+    while(!accnt && i<3){
+        console.log("enter your credentials\n");
+        email=prompt("email adress:\n");
+        password=prompt("email adress:\n");
+        accnt=account.managerLogin(email,password);
+
+        if(accnt){
+            i=3;
+        }
+        else{
+            console.log("wrong credentials");
+            i++;
+        }
+    }
+    let again='yes';
+    while(again=='yes'){
+        if(accnt){
+            const manager=new Manager();
+            manager.hotel_organize();
+            console.log('Do you want to add another room information\n');
+            again=prompt('yes/no\n');
+        }
+    }
+    
+
 } else if (answer == 2) {
     console.log("Welcome to Hotel Agadir, to browse our menu you can choose one of the following options:\n");
     let account_choice = prompt("1)login\n2)Create an account\n");
@@ -36,13 +50,11 @@ if (answer == 1) {
     let answ = false;
 
     if (account_choice == 2) {
-        // Create account logic
         account.create_account(password, email);
         created = true;
-        // Save user information
-        saveUserInfo(email, password);
-    } else if (account_choice == 1) {
-        // Login logic
+        
+    } 
+    else if (account_choice == 1) {
         let i = 0;
         while (i < 3) {
             answ = account.login(password, email);
@@ -57,7 +69,6 @@ if (answer == 1) {
     }
 
     if (answ == true && created == true) {
-        // Client reservation logic
         const rm = new Room();
         console.log("Enter the following information about yourself:\n");
         let fname = prompt('First name:\n');
@@ -66,6 +77,8 @@ if (answer == 1) {
         let email_address = prompt('Email address;\n');
         let address_info = prompt('Address information:\n');
         const cl = new Client(fname, lname, age, email_address, address_info);
-        let f = cl.make_reservation();
+        let room_number = cl.make_reservation();
+        rm.edit(room_number,true);
+        
     }
 }
